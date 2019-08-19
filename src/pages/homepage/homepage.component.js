2d8f6fc87983cd8db7b1ff65d63e5b5edae6a192
jmarellanes <dev.jmarellanes@gmail.com>
2019-08-19 00:20:28 -0600

    Refactoring header and homepage components to use styled-components

--- src/pages/homepage/homepage.component.js
+++ src/pages/homepage/homepage.component.js
@@ -2,12 +2,12 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-import './homepage.styles.scss';
+import { HomePageContainer } from './homepage.styles';
 
-const Homepage = () => (
-  <div className='homepage'>
+const HomePage = () => (
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 );
 
-export default Homepage;
+export default HomePage;
